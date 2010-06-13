86dba2cdeef22dbfbafb2ac88a26fc3946bfc52e
Michael Stevens <mstevens@etla.org>
2010-06-13 19:07:43 +0100

    v1.01

--- lib/Proc/InvokeEditor.pm
+++ lib/Proc/InvokeEditor.pm
@@ -30,7 +30,7 @@ use vars qw(@ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION
 @EXPORT = qw(
 	
 );
-$VERSION = '1.00';
+$VERSION = '1.01';
 
 @DEFAULT_EDITORS = ( $ENV{'VISUAL'}, $ENV{'EDITOR'}, '/usr/bin/vi',
                      '/bin/vi', '/bin/ed'
